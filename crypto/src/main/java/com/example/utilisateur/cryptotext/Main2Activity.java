d9c4408d2661a0c497b3a5486bfb133a7f918e01
DonatienTERTRAIS <donatien.tertrais@ensta-bretagne.org>
2016-09-09 16:13:50 +0200

    Beginning, unfunctionnal

---
+++ crypto/src/main/java/com/example/utilisateur/cryptotext/Main2Activity.java
@@ -0,0 +1,37 @@
+package com.example.utilisateur.cryptotext;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    public void sendMessage(){
+        EditText txtphoneNo = (EditText) findViewById(R.id.phoneNumber);
+        EditText txtMessage = (EditText) findViewById(R.id.message);
+
+        Log.i("Send SMS", "");
+        String phoneNo = txtphoneNo.getText().toString();
+        String message = txtMessage.getText().toString();
+
+        try {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(phoneNo, null, message, null, null);
+            Toast.makeText(getApplicationContext(), "SMS sent.", Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "SMS faild, please try again.", Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+    }
+}
