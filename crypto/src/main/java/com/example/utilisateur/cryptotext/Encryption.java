8476069f50fce7d3053e1d4c710fdc12e5bc1158
DonatienTERTRAIS <donatien.tertrais@ensta-bretagne.org>
2016-09-14 19:16:46 +0200

    To be done: conversation activity

--- crypto/src/main/java/com/example/utilisateur/cryptotext/Encryption.java
+++ crypto/src/main/java/com/example/utilisateur/cryptotext/Encryption.java
@@ -2,7 +2,17 @@ package com.example.utilisateur.cryptotext;
 
 import android.util.Base64;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
@@ -12,28 +22,71 @@ import javax.crypto.spec.SecretKeySpec;
 /**
  * Created by DonatienTERTRAIS on 10/09/2016.
  */
+
+//The keys are stocked in a file, this file is protected with a password which is to be given every time a key is used (before openning and creating a conversation
+
+
+
 public class Encryption {
+    private static final String keyStoreFile = "/storage/emulated/CryptoText/keys.keystore";
+    private static final Level level = Level.WARNING;
+    protected static Logger logger = Logger.getLogger(Encryption.class.getName());
+
+    public static KeyStore createKeyStore(String filePassword) {
+        File file = new File(keyStoreFile);
+
+        KeyStore keyStore = null;
+        try {
+            keyStore = KeyStore.getInstance("JCEKS");
+        } catch (KeyStoreException e) {
+            logger.log(level, e.toString());
+        }
+        if (file.exists()) {
+            // .keystore file already exists => load it
+            try {
+                keyStore.load(new FileInputStream(file), filePassword.toCharArray());
+            } catch ( NoSuchAlgorithmException e) {
+                logger.log(level, e.toString());// No algorithm to check the keystore integrity
+            } catch ( CertificateException e) {
+                logger.log(level, e.toString());// KeyStore certificates cannot be loaded
+            } catch ( IOException e) {
+                logger.log(level, e.toString());// Error keystore file password
+            }
+        } else {
+            // .keystore file not created yet => create it
+            try {
+                keyStore.load(null, null);
+                keyStore.store(new FileOutputStream(keyStoreFile), filePassword.toCharArray());
+            } catch ( NoSuchAlgorithmException e) {
+                logger.log(level, e.toString());// The algorithm to check the integrity cannot be found
+            } catch ( CertificateException e) {
+                logger.log(level, e.toString());// The certificate cannot be stored
+            } catch ( IOException e) {
+                logger.log(level, e.toString());
+            } catch ( KeyStoreException e) {
+                logger.log(level, e.toString());// KeyStore not loaded
+            }
+        }
+        return keyStore;
+    }
 
     // Encrypts string and encodes in Base64
-    public static String encrypt( String password, String data ) throws Exception
-    {
-        byte[] secretKey = generateKey( password.getBytes() );
+    public static String encrypt( String password, String data ) throws Exception {
+        byte[] secretKey = password.getBytes();
         byte[] clear = data.getBytes();
-
         SecretKeySpec secretKeySpec = new SecretKeySpec( secretKey, "AES" );
         Cipher cipher = Cipher.getInstance("AES" );
         cipher.init( Cipher.ENCRYPT_MODE, secretKeySpec );
 
-        byte[] encrypted = cipher.doFinal( clear );
+        byte[] encrypted = cipher.doFinal(clear);
         String encryptedString = Base64.encodeToString(encrypted, Base64.DEFAULT);
 
         return encryptedString;
     }
 
     // Decrypts string encoded in Base64
-    public static String decrypt( String password, String encryptedData ) throws Exception
-    {
-        byte[] secretKey = generateKey(password.getBytes());
+    public static String decrypt( String password, String encryptedData ) throws Exception {
+        byte[] secretKey = password.getBytes();
         SecretKeySpec secretKeySpec = new SecretKeySpec( secretKey, "AES" );
         Cipher cipher = Cipher.getInstance( "AES" );
         cipher.init( Cipher.DECRYPT_MODE, secretKeySpec );
@@ -44,13 +97,30 @@ public class Encryption {
         return new String(decrypted);
     }
 
-    public static byte[] generateKey( byte[] seed ) throws Exception
-    {
-        KeyGenerator keyGenerator = KeyGenerator.getInstance( "AES" );
-        SecureRandom secureRandom = SecureRandom.getInstance( "SHA1PRNG" );
-        secureRandom.setSeed( seed );
-        keyGenerator.init( 128, secureRandom );
-        SecretKey secretKey = keyGenerator.generateKey();
-        return secretKey.getEncoded();
+    public static SecretKey generateKey( byte[] seed ) {
+        KeyGenerator keyGenerator;
+        SecureRandom secureRandom;
+        try {
+            keyGenerator = KeyGenerator.getInstance( "AES" );
+            secureRandom = SecureRandom.getInstance( "SHA1PRNG" );
+            secureRandom.setSeed( seed );
+            keyGenerator.init( 128, secureRandom );
+            SecretKey secretKey = keyGenerator.generateKey();
+            return secretKey;
+        } catch (NoSuchAlgorithmException e) {
+            logger.log(level, e.toString());// Unknown algorithm for key/random number generation
+            return null;
+        }
+    }
+
+    public static boolean isStocked(String entry, String filePassword) {
+        KeyStore keyStore = createKeyStore(filePassword);
+        boolean cond = false;
+        try {
+            cond = keyStore.containsAlias(entry);
+        } catch (KeyStoreException e) {
+            logger.log(level, e.toString());
+        }
+        return cond;
     }
 }
---
+++ crypto/src/main/java/com/example/utilisateur/cryptotext/ModifyConversationActivity.java
@@ -0,0 +1,138 @@
+package com.example.utilisateur.cryptotext;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.PhoneNumberUtils;
+import android.telephony.TelephonyManager;
+import android.view.View;
+import android.widget.EditText;
+//import com.google.i18n.phonenumbers.PhoneNumberUtil;
+
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * @author DonatienTERTRAIS
+ */
+public class ModifyConversationActivity extends AppCompatActivity {
+    private String phoneN;
+    private static final Level level = Level.WARNING;
+    private static Logger logger = Logger.getLogger(Encryption.class.getName());
+    private EditText phone = (EditText) findViewById(R.id.phone);
+    private EditText receptionKeySeed = (EditText) findViewById(R.id.receptionKeySeed);
+    private EditText emissionKeySeed = (EditText) findViewById(R.id.emissionkeySeed);
+    private EditText keyStoreField = (EditText) findViewById(R.id.keyStorePassword);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modifyconversation);
+
+        if (savedInstanceState == null) {
+            Bundle extras = getIntent().getExtras();
+            if(extras != null) {
+                phoneN = (String) extras.get("phoneNumber");
+            }
+        } else {
+            phoneN = (String) savedInstanceState.getSerializable("phoneNumber");
+        }
+        if (phoneN.length() != 0) {
+            phone.setText((phoneN));
+            phone.setEnabled(false);
+        }
+    }
+
+    public void createConversation(View view){
+        String phoneNumber = phone.getText().toString();
+        String emissionSeed = emissionKeySeed.getText().toString();
+        String receptionSeed = receptionKeySeed.getText().toString();
+        String keyStorePassword = keyStoreField.getText().toString();
+        phoneNumber = formatNumber(phoneNumber);
+        String errors ="";
+        if (phoneNumber == null) {
+            errors = errors + "The phone number isn't correct\n";
+        } else {
+            if (emissionSeed.length() != 0 ^ Encryption.isStocked(phoneNumber + "Out", keyStorePassword)) {
+                if (emissionSeed.length() != 0) {
+                    errors = errors + "The current emission seed will be overwritten\n";
+                } else {
+                    errors = errors + "The emission seed must be set\n";
+                }
+            } if (receptionSeed.length() != 0 ^ Encryption.isStocked(phoneNumber + "In", keyStorePassword)) {
+                if (receptionSeed.length() != 0) {
+                    errors = errors + "The current reception seed will be overwritten\n";
+                } else {
+                    errors = errors + "The reception seed must be set\n";
+                }
+            }
+        }
+        if (errors.length() == 0) {
+            saveSeeds();
+        } else {
+            createAlertDialog(errors);
+        }
+    }
+
+    public void saveSeeds() {
+        String phoneNumber = phone.getText().toString();
+        String emissionSeed = emissionKeySeed.getText().toString();
+        String receptionSeed = receptionKeySeed.getText().toString();
+        String keyStorePassword = keyStoreField.getText().toString();
+        KeyStore keyStore = Encryption.createKeyStore(keyStorePassword);
+        KeyStore.PasswordProtection passwordProtection = new KeyStore.PasswordProtection(keyStorePassword.toCharArray());
+        try {
+            if (emissionSeed.length() != 0) {
+                KeyStore.SecretKeyEntry keyStoreEntry = new KeyStore.SecretKeyEntry(Encryption.generateKey(emissionSeed.getBytes()));
+                keyStore.setEntry(phoneNumber + "Out", keyStoreEntry, passwordProtection);
+            }
+            if (receptionSeed.length() != 0) {
+                KeyStore.SecretKeyEntry keyStoreEntry = new KeyStore.SecretKeyEntry(Encryption.generateKey(receptionSeed.getBytes()));
+                keyStore.setEntry(phoneNumber + "In", keyStoreEntry, passwordProtection);
+            }
+        } catch (KeyStoreException e) {
+            logger.log(level, e.toString());// Keystore not loaded
+        }
+        Intent intent = new Intent(this, Conversation.class);
+        intent.putExtra("phoneNumber", phone.getText());
+        startActivity(intent);
+    }
+
+    public void createAlertDialog (String errors) {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Warnings");
+        alert.setMessage("Warnings :\n" + errors);
+        alert.setPositiveButton("Continue", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                saveSeeds();
+            }
+        });
+        alert.setNegativeButton("Go Back", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        alert.show();
+    }
+
+    private String formatNumber(String phoneNumber) {
+        TelephonyManager tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        String iso = tm.getSimCountryIso();
+        if (!phoneNumber.startsWith("+")) {
+            phoneNumber = "+" + CountryToPhonePrefix.prefixFor(iso.toUpperCase()) + phoneNumber;
+        }
+        if (android.os.Build.VERSION.SDK_INT >= 21) {
+            return PhoneNumberUtils.formatNumberToE164(phoneNumber, iso);
+        } else {
+            return PhoneNumberUtils.formatNumber(phoneNumber);
+        }
+    }
+}
