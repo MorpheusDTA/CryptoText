2a77cb033b3daa91dd18f9293bc8f0e679b53431
DonatienTERTRAIS <donatien.tertrais@ensta-bretagne.org>
2016-10-10 18:33:57 +0200

    Conversation is started, test undone

--- crypto/src/main/java/com/example/utilisateur/cryptotext/Conversation.java
+++ crypto/src/main/java/com/example/utilisateur/cryptotext/Conversation.java
@@ -6,7 +6,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.AdapterView;
