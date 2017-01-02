d922230c6bb78c9596edf298557ba8f7b333e24f
Donatien TERTRAIS <donatien.tertrais@ensta-bretagne.org>
2017-01-02 10:41:22 +0100

    Event system deleted, notification to be implemented, deletion of sms/conversations to be implemented, receive/send tested and ok

--- crypto/src/main/java/com/example/utilisateur/cryptotext/SmsReceiver.java
+++ crypto/src/main/java/com/example/utilisateur/cryptotext/SmsReceiver.java
@@ -35,16 +35,14 @@ public class SmsReceiver extends BroadcastReceiver {
     // Change the password here or give a user possibility to change it
     //public static final byte[] PASSWORD = new byte[]{ 0x20, 0x32, 0x34, 0x47, (byte) 0x84, 0x33, 0x58 };
 
-
-    /*@Override
     public void onReceive( Context context, Intent intent ) {
-        String address;
-        String title = "";
-        SmsMessage[] msgs = Intents.getMessagesFromIntent(intent);
+        String address = "";
+        String text = "";
         // Get ContentResolver object for pushing SMS to the incoming folder
-        ContentResolver contentResolver = this.context.getContentResolver();
+        ContentResolver contentResolver = context.getContentResolver();
 
-        if ( msgs.length == 1  ) {// only one new message
+        SmsMessage[] msgs = Intents.getMessagesFromIntent(intent);
+        if (msgs.length == 1) {
             SmsMessage sms = msgs[0];
             address = sms.getOriginatingAddress();
 
@@ -55,47 +53,17 @@ public class SmsReceiver extends BroadcastReceiver {
                 address = c.getString(c.getColumnIndex(ContactsContract.PhoneLookup.DISPLAY_NAME));
             }
             c.close();
-            title = "SMS from " + address;
+            text = "SMS from " + address + " :\n";
 
             putSmsToDatabase( contentResolver, sms );
-        } else if (msgs.length != 0) {// a list of new messages, cretaion of the adequate notification
-            title = "" + msgs.length + " new messages";
-            for (SmsMessage sms :  msgs) {
-                putSmsToDatabase( contentResolver, sms );
-            }
-        }
-
-        Toast.makeText(context, title, Toast.LENGTH_SHORT).show();
-        //this.abortBroadcast();
-    }*/
-
-    public void onReceive( Context context, Intent intent ) {
-        String messages = "";
-
-        SmsMessage[] msgs = Intents.getMessagesFromIntent(intent);
-        if ( msgs.length != 0) {
-            // Get ContentResolver object for pushing SMS to the incoming folder
-            ContentResolver contentResolver = context.getContentResolver();
-
+        } else if ( msgs.length > 1) {
             for (SmsMessage sms:msgs) {
-                String address = sms.getOriginatingAddress();
-
-                //Resolving the contact name from the contacts.
-                Uri lookupUri = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI, Uri.encode(address));
-                Cursor c = contentResolver.query(lookupUri, new String[]{ContactsContract.Data.DISPLAY_NAME}, null, null, null);
-                if (c != null && c.moveToFirst()) {
-                    address = c.getString(c.getColumnIndex(ContactsContract.PhoneLookup.DISPLAY_NAME));
-                }
-                c.close();
-                messages += "SMS from " + address + " :\n";
-
                 putSmsToDatabase( contentResolver, sms );
             }
-
-            // Display SMS message
-            Toast.makeText(context, messages, Toast.LENGTH_SHORT).show();
+            text = "" + msgs.length + "new messages";
         }
 
+        Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
         // this.abortBroadcast();
     }
 
